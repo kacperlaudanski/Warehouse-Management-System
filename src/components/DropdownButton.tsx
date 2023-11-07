099f9374043ed3f2778e5c8e77f446f8c20f20bb
Kacper <kacper.laudanski@wp.pl>
2023-11-07 11:32:59 +0100

    dropdown menu styling

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -1,14 +1,17 @@
+import { ScanEye } from "lucide-react";
 import { ReactNode, useState } from "react";
 import Button from "./Button";
 
 interface DropdownButton {
   isSidebarShown: boolean;
-  children: any;
+  children: ReactNode;
+  menuItems: { link: string; name: string }[] | undefined;
 }
 
 export default function DropdownButton({
   isSidebarShown,
   children,
+  menuItems,
 }: DropdownButton) {
   const [isDropdownShown, setDropdown] = useState(false);
   return (
@@ -20,9 +23,14 @@ export default function DropdownButton({
         {children}
       </Button>
       {isDropdownShown && (
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
+        <ul className={`w-2/3 h-auto flex flex-col border-l-2 border-solid mt-2 mb-2 ${!isSidebarShown && 'hidden'}`}>
+          {menuItems?.map((item) => { 
+            return (
+              <li className="ml-2">
+                <Button sidebar='extended' onClick={() => window.location.pathname = item.link}>{item.name}</Button>
+              </li>
+            );
+          })}
         </ul>
       )}
     </>
