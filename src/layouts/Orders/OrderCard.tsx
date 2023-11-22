1873931cd9b00954a9e69d5d2ea27acf6cf3f25c
Kacper <kacper.laudanski@wp.pl>
2023-11-22 11:51:16 +0100

    order cart component

--- src/layouts/Orders/OrderCard.tsx
+++ src/layouts/Orders/OrderCard.tsx
@@ -1,9 +1,54 @@
-export default function OrderCard(){
-    return (
-        <>
-          <div className="w-64 h-80 bg-neutral-200 m-4">
+import { X } from "lucide-react";
+import Button from "../../components/Button";
 
-          </div>
-        </>
-    )
+interface Order {
+  orderID: string;
+  orderer: string;
+  address: string;
+  date: string;
+}
+
+export default function OrderCard({ orderID, orderer, address, date }: Order) {
+  return (
+    <>
+      <div className="w-72 h-min-96 bg-neutral-100 m-4 p-4 rounded-xl flex flex-col justify-between items-start">
+        <div className="w-full flex justify-between items-center">
+          <h2 className="text-xl mb-2">Order: {orderID}</h2>
+          <Button variant="ghost">
+            <X />
+          </Button>
+        </div>
+        <div className="w-full h-full flex flex-col justify-between items-start">
+          <span className="text-sm">Orderer: {orderer}</span>
+          <span className="text-sm">Address: {address}</span>
+          <span className="text-sm">Date: {date}</span>
+          <table className="w-full border-1 border-solid border-neutral-500 rounded-xl table-fixed my-5">
+            <tbody>
+              <tr className="bg-blue-300">
+                <td className="border-1 border-neutral-500 p-2">Product</td>
+                <td className="border-1 border-neutral-500 p-2">ID</td>
+                <td className="border-1 border-neutral-500 p-2">Location</td>
+              </tr>
+              <tr>
+                <td className="border-1 border-neutral-500 p-2">Cups</td>
+                <td className="border-1 border-neutral-500 p-2">48756</td>
+                <td className="border-1 border-neutral-500 p-2">R54</td>
+              </tr>
+              <tr>
+                <td className="border-1 border-neutral-500 p-2">Cups</td>
+                <td className="border-1 border-neutral-500 p-2">48756</td>
+                <td className="border-1 border-neutral-500 p-2">R54</td>
+              </tr>
+              <tr>
+                <td className="border-1 border-neutral-500 p-2">Cups</td>
+                <td className="border-1 border-neutral-500 p-2">48756</td>
+                <td className="border-1 border-neutral-500 p-2">R54</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <Button orders="complete">Complete an order</Button>
+      </div>
+    </>
+  );
 }
--- src/pages/OrdersPreview.tsx
+++ src/pages/OrdersPreview.tsx
@@ -7,11 +7,11 @@ export default function OrdersPreview() {
       <div className="w-full h-screen bg-white p-8 rounded-tl-3xl rounded-bl-3xl flex flex-col overflow-y-hidden">
         <h1 className="text-2xl tracking-wider">Orders | Preview</h1>
         <div className="w-max h-full grid auto-rows-auto grid-cols-4 mt-8">
-          <OrderCard /> 
-          <OrderCard /> 
-          <OrderCard /> 
-          <OrderCard /> 
-          <OrderCard /> 
+          <OrderCard orderID="RS3535" orderer="John Kovalsky" address="Hollywood Blvd 1234" date="11.11.2023"/> 
+          <OrderCard orderID="RS3535" orderer="John Kovalsky" address="Hollywood Blvd 1234" date="11.11.2023"/> 
+          <OrderCard orderID="RS3535" orderer="John Kovalsky" address="Hollywood Blvd 1234" date="11.11.2023"/> 
+          <OrderCard orderID="RS3535" orderer="John Kovalsky" address="Hollywood Blvd 1234" date="11.11.2023"/> 
+          <OrderCard orderID="RS3535" orderer="John Kovalsky" address="Hollywood Blvd 1234" date="11.11.2023"/> 
         </div>
       </div>
     </Main>
