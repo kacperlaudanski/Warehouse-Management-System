57f152dd66dfbf55faecc97b60a8b63a89cabe73
Kacper <kacper.laudanski@wp.pl>
2023-11-23 20:40:25 +0100

    order history page

---
+++ src/layouts/Orders/HistoryTable.tsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '../../components/Button';
+import { Edit, X } from 'lucide-react';
+import { flexbox } from '@mui/system';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.grey[500],
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(
+  number: number,
+  id: string,
+  client: string,
+  address: string,
+  status: string,
+  date: string, 
+) {
+  return { number, id, client, address, status, date };
+}
+
+const rows = [
+  createData(1, 'rui47579', 'Jan Kowalski', 'Klonowa 24', 'Pending', '11.11.2002'),
+  createData(2, 'rui47579', 'Jan Kowalski', 'Klonowa 24', 'Pending','11.11.2002'),
+  createData(3, 'rui47579', 'Jan Kowalski', 'Klonowa 24', 'Pending','11.11.2002'),
+  createData(4, 'rui47579', 'Jan Kowalski', 'Klonowa 24', 'Pending','11.11.2002'),
+  createData(5, 'rui47579', 'Jan Kowalski', 'Klonowa 24', 'Pending','11.11.2002'),
+];
+
+export default function CustomizedTables() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>No</StyledTableCell>
+            <StyledTableCell align="center">Order ID</StyledTableCell>
+            <StyledTableCell align="center">Client</StyledTableCell>
+            <StyledTableCell align="center">Address</StyledTableCell>
+            <StyledTableCell align="center">Status</StyledTableCell>
+            <StyledTableCell align="center">Date</StyledTableCell>
+            <StyledTableCell align="center">Action</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.number}>
+              <StyledTableCell component="th" scope="row">
+                {row.number}
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.id}</StyledTableCell>
+              <StyledTableCell align="center">{row.client}</StyledTableCell>
+              <StyledTableCell align="center">{row.address}</StyledTableCell>
+              <StyledTableCell align="center">{row.status}</StyledTableCell>
+              <StyledTableCell align="center">{row.date}</StyledTableCell>
+              <StyledTableCell align="center"><Button variant='ghost' icon='actionButton'><Edit/></Button></StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
