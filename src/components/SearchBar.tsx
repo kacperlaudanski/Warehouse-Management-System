5f1a4d730fe35d33d4dffcb9e111d53aac6a5a4c
Kacper <kacper.laudanski@wp.pl>
2023-12-03 10:40:49 +0100

    responsive search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@ import Input from "./Input";
 
 export default function SearchBar() {
   return (
-    <div className="w-64 h-15 flex justify-center items-center border-solid border-neutral-500 border-2 rounded-2xl text-neutral-400">
+    <div className="sm:w-56 lg:w-64 h-15 flex justify-center items-center border-solid border-neutral-500 border-2 rounded-2xl text-neutral-400">
       <Search />
       <Input type="search" variant="search" placeholder="Search..." />
     </div>
