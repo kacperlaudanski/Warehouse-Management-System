470dbdc28524c59d9776bb426e8460ad3081c2d7
Kacper <kacper.laudanski@wp.pl>
2024-01-09 19:45:27 +0100

    landing page design

--- src/components/LandingPage/Navbar.tsx
+++ src/components/LandingPage/Navbar.tsx
@@ -1,15 +1,12 @@
-import { useNavigate, NavLink } from "react-router-dom";
-import Button from "../Button";
+import NavbarChild from "./NavbarChild";
 
 export default function Navbar(){
-    const navigate = useNavigate(); 
     return (
-        <nav className="w-2/5 h-full flex justify-between items-center mx-8">
-          <ul className="w-full h-full flex justify-between items-center text-white">
-            <li><NavLink to={'javascript:void(0);'}>About Us</NavLink></li>
-            <li><NavLink to={'javascript:void(0);'}>Products</NavLink></li>
-            <li><NavLink to={'javascript:void(0);'}>Contact</NavLink></li>
-            <Button variant='navbarLogin' onClick={() => navigate('./login')}>Login</Button>
+        <nav className="w-3/5 h-full flex justify-between items-center mx-8">
+          <ul className="w-full h-full flex justify-between items-center text-neutral-500 child">
+            <NavbarChild name="About Us"/>
+            <NavbarChild name="Products"/>
+            <NavbarChild name="Contact" /> 
           </ul>
         </nav>
     )
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,39 @@
-import Header from "../layouts/Header"
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Navbar from "../components/LandingPage/Navbar";
+import SocialMediaPanel from "../components/LandingPage/SocialMediaPanel";
+import Logo from "../images/logo-dark-trans.png";
 
-export default function LandingPage(){
-    return (
-        <Header />
-    )
+export default function LandingPage() {
+  const navigate = useNavigate();
+  return (
+    <main className="w-full h-screen flex bg-white">
+      <section className="w-2/4 h-full p-20 flex flex-col justify-between bg-white">
+        <header className="w-full h-auto flex justify-between items-center">
+          <img src={Logo} alt="logo" className="w-24" />
+          <Navbar /> 
+        </header>
+        <div className="w-3/5 h-full flex flex-col items-start justify-between my-20">
+          <h1 className="text-6xl mb-10 leading-10">
+            Effortless{" "}
+            <span className="mx-5 text-indigo-600">Warehouse Management</span>{" "}
+            at Your <span className="mx-5 text-red-400">Fingertips</span>
+          </h1>
+          <p className="my-10 text-neutral-500">
+            Simplify warehouse management with WarehouseWiz. Effortlessly
+            optimize your inventory. Your warehouse, simplified.
+          </p>
+          <Button action="getStarted" onClick={() => navigate("/register")}>
+            Get Started
+          </Button>
+        </div>
+        <SocialMediaPanel />
+      </section>
+      <section className="w-2/4 h-full flex flex-col items-end border-8 border-solid border-white rounded-l-3xl bg-warehouse-pattern bg-no-repeat bg-center">
+        <div className="w-48 h-28 flex items-center justify-center bg-white rounded-l-full relative before:content-['*'] before:w-20 before:h-20 before:bg-white before:absolute before:left-0 before:bottom-2/4">
+          <Button action="landingLogin" onClick={() => navigate('/login')}>Login</Button>
+        </div> 
+      </section>
+    </main>
+  );
 }
